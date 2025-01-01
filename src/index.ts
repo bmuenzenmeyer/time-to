9430318ab6e8b52c55f67ec472f1e1a8cad53832
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2025-01-01 06:01:30 +0000

    feat: add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+#!/usr/bin/env node
+
 import chalk from 'chalk'
 import 'dotenv/config'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 const {LOCALE} = process.env
 
-console.log(chalk.bgBlue('time-to!!!'))
+const options = yargs(hideBin(process.argv))
+.option('to', {
+    type: 'string',
+    description: 'Date string to measure time until'
+  })
+.option('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'Run with verbose logging'
+  })
+.parseSync()
+
+console.log(chalk.bgBlue(`time-to ${options.to}`))
 console.log({LOCALE})
