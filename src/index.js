088676a41b1ff7e95121c2b2a9260d76d68425a6
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2025-01-02 05:20:56 +0000

    cleanup, switching all to node

--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 #!/usr/bin/env node
 
-// import { parseArgs } from 'node:util'
-
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+import { parseArgs } from 'node:util'
 
 import { calculateTimeFromNowTo } from './lib/calculate.js'
 
-const values = yargs(hideBin(process.argv))
-	.option('to', {
-		type: 'string',
-		description: 'Date string to measure time until',
-	})
-	.option('verbose', {
-		alias: 'v',
-		type: 'boolean',
-		description: 'Run with verbose logging',
-	})
-	.parseSync()
-
-// const { values } = parseArgs({
-// 	options: {
-// 		to: {
-// 			type: 'string',
-// 			description: 'Date string to measure time until',
-// 		},
-// 	},
-// })
+const { values } = parseArgs({
+	options: {
+		to: {
+			type: 'string',
+			description: 'Date string to measure time until',
+		},
+	},
+})
 
 const timeTo = calculateTimeFromNowTo(values.to)
 console.log(timeTo)
--- src/lib/__tests__/calculate.test.js
+++ src/lib/__tests__/calculate.test.js
@@ -1,12 +1,9 @@
-// import { describe, it } from 'node:test'
+import { describe, it } from 'node:test'
 import { createUTCDate } from '../calculate.js'
 
 describe('createUTCDate', () => {
-	it('should create a date in UTC time', (
-		// test
-	) => {
+	it('should create a date in UTC time', (test) => {
 		const date = createUTCDate(2026, 0, 1)
-		expect(date.toISOString()).toEqual('2026-01-01T00:00:00.000Z')
-		// test.assert.strictEqual(date.toISOString(), '2026-01-01T00:00:00.000Z')
+		test.assert.strictEqual(date.toISOString(), '2026-01-01T00:00:00.000Z')
 	})
 })
