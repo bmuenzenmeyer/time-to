921b0892d91b07051eb86f9b8207e46fb1a007e7
bmuenzenmeyer <brian.muenzenmeyer@gmail.com>
2025-05-02 17:22:01 -0500

    cleanup for demo

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,14 @@
 import { createServer } from 'node:http'
-import { parseArgs, styleText } from 'node:util'
+import { styleText } from 'node:util'
 
 import { calculateTimeFromNowTo } from './lib/calculate.js'
 
 const PORT = Number(process.env.PORT)
 
-const { values } = parseArgs({
-	options: {
-		to: {
-			type: 'string',
-			description: 'Date string to measure time until',
-		},
-	},
-})
 
 const server = createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain' })
-	res.end(calculateTimeFromNowTo(new Date(values.to || '2026-01-01')))
+	res.end(calculateTimeFromNowTo('2025-12-31'))
 })
 
 server.listen(PORT, '127.0.0.1', () => {
