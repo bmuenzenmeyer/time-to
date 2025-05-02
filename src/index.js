82f2ab4b09f2ed4fbf88df4244aec92043e4a6fa
bmuenzenmeyer <brian.muenzenmeyer@gmail.com>
2025-05-02 17:21:34 -0500

    cleanup for demo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-// import { parseArgs } from 'node:util'
-
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
@@ -12,21 +10,7 @@ const values = yargs(hideBin(process.argv))
 		type: 'string',
 		description: 'Date string to measure time until',
 	})
-	.option('verbose', {
-		alias: 'v',
-		type: 'boolean',
-		description: 'Run with verbose logging',
-	})
 	.parseSync()
-
-// const { values } = parseArgs({
-// 	options: {
-// 		to: {
-// 			type: 'string',
-// 			description: 'Date string to measure time until',
-// 		},
-// 	},
-// })
 
 const timeTo = calculateTimeFromNowTo(values.to)
 console.log(timeTo)
